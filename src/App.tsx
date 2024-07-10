222d2b4d7a8c5b6443d37b8d34dc8061ad68912b
sonih0005 <sonih0005@gmail.com>
2024-07-10 18:08:37 +0530

    created pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import AuthLayout from "./_auth/AuthLayout";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
-import { Home } from "./_root/Pages/index";
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from "./_root/Pages/index";
 import RootLayout from "./_root/RootLayout";
 
 import "./globals.css";
@@ -21,6 +21,14 @@ function App() {
         {/* {private routes} */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element= {<Explore />}/>
+          <Route path="/saved" element= {<Saved />}/>
+          <Route path="/all-users" element= {<AllUsers />}/>
+          <Route path="/create-post" element= {<CreatePost />}/>
+          <Route path="/update-post/:id" element= {<EditPost />}/>
+          <Route path="/posts/:id" element= {<PostDetails />}/>
+          <Route path="/profile/:id/*" element= {<Profile />}/>
+          <Route path="/update-profile/:id" element= {<UpdateProfile />}/>
         </Route>
       </Routes>
       <Toaster />
--- src/_root/Pages/index.ts
+++ src/_root/Pages/index.ts
@@ -1,5 +1,21 @@
 import Home from "./Home";
+import Explore from "./Explore";
+import AllUsers from "./AllUsers";
+import Saved from "./Saved";
+import CreatePost from "./CreatePost";
+import EditPost from "./EditPost";
+import PostDetails from "./PostDetails";
+import Profile from "./Profile";
+import UpdateProfile from "./UpdateProfile";
 
 export  {
-    Home
+    Home,
+    Explore,
+    AllUsers,
+    Saved,
+    CreatePost,
+    EditPost,
+    PostDetails,
+    Profile,
+    UpdateProfile,
 }
